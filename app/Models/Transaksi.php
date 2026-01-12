748d3db857c12ed087b4b59c8a959cbdec5457be
BungaDoR <oktaviab14@gmail.com>
2026-01-12 22:01:05 +0700

    Perbaikan typo

--- app/Models/Transaksi.php
+++ app/Models/Transaksi.php
@@ -8,6 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Transaksi extends Model
 {
     use HasFactory;
+    
+    protected $table = 'transaksi';
 
     protected $fillable = [
         'user_id',
