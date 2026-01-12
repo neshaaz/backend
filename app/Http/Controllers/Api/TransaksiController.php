2fdd9bef2033eda179cc01d9d6ca1f707a3c8d68
BungaDoR <oktaviab14@gmail.com>
2026-01-12 20:05:12 +0700

    membuat fungsi untuk menampilkan "List Transaksi"-TransaksiController

--- app/Http/Controllers/Api/TransaksiController.php
+++ app/Http/Controllers/Api/TransaksiController.php
@@ -9,4 +9,27 @@ use Illuminate\Http\Request;
 class TransaksiController extends Controller
 {
     // List Transaksi
+    public function index(Request $request)
+    {
+        $query = Transaksi::where('user_id', $request->user()->id);
+
+        // Jenis transaksi (pemasukan / pengeluaran)
+        if ($request->jenis) {
+            $query->where('type', $request->jenis);
+        }
+
+        // Jenis kategori
+        if ($request->kategori) {
+            $query->where('kategori', $request->kategori);
+        }
+
+        $transaksi = $query
+            ->orderBy('tanggal', 'desc')
+            ->get();
+
+            return response()->json([
+                'status' => true,
+                'data' => $transaksi
+            ]);
+    }
 }
